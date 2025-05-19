7b51029ae0649fc7845483e3b93e6fea2c7ee8da
joydeep ganguly <joydeepganguly32@gmail.com>
2025-05-19 15:38:02 +0530

    Reintroduced standardscaler

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import pickle
-#from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 from flask import Flask, render_template, request
 
 model = pickle.load(open('delivery_promptness_rf.pkl','rb'))
@@ -12,10 +12,11 @@ def index():
     if request.method=='GET':
         return render_template('index.html')
     else:
-        #ss = StandardScaler()
+        ss = StandardScaler()
         form_inputs = pd.DataFrame(request.form.to_dict(),index=[0])
-        #form_inputs = ss.fit_transform(form_inputs)
-        prediction = model.predict(form_inputs.astype('float'))
+        form_inputs = form_inputs.astype('float')
+        form_inputs = ss.fit_transform(form_inputs)
+        prediction = model.predict(form_inputs)
         return str(prediction)
         #return request.form.to_dict()
 
